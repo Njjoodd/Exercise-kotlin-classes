95dac4ca7347c03b161aeaa4a880dae88467ed91
Njood <nejood03@gmail.com>
2025-02-17 10:17:24 +0300

    step 3 and the bonus has been complete, adding a function and adding genre for the bonus

--- Classes/src/kotlin_classes.kt
+++ Classes/src/kotlin_classes.kt
@@ -1,7 +1,10 @@
-class Book(val title: String, val author: String, val yearPublished: Int)
+class Book(val title: String, val author: String, val yearPublished: Int, val genre: String) {
 
+    fun getBookInfo () : String {
+        return "$title - $author ($yearPublished) - Genre: $genre"
+    }
+}
 fun main() {
-    val book = Book("The Starless Sea", "Erin Morgenstern", 2019)
-
-    println("The book is called ${book.title} by ${book.author}, and was published in ${book.yearPublished}")
+    val book = Book("The Starless Sea", "Erin Morgenstern", 2019, "Magical Realism")
+    println(book.getBookInfo())
 }
