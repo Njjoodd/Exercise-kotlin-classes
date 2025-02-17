e4468f5f99ad748622a16b938fdfb634321010ab
Njood <nejood03@gmail.com>
2025-02-17 10:16:09 +0300

    step 2 in classes completed (adding a constructor) ..

--- Classes/src/kotlin_classes.kt
+++ Classes/src/kotlin_classes.kt
@@ -1,15 +1,7 @@
-class Book {
-    var title: String = ""
-    var author: String = ""
-    var yearPublished: Int = 0
-}
+class Book(val title: String, val author: String, val yearPublished: Int)
 
 fun main() {
-    val book = Book()
-    book.title = "The starless sea"
-    book.author = "Erinn Morgenstrern"
-    book.yearPublished = 2019
+    val book = Book("The Starless Sea", "Erin Morgenstern", 2019)
 
     println("The book is called ${book.title} by ${book.author}, and was published in ${book.yearPublished}")
 }
-
